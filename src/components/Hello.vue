8a4ec4da3a2adbbe68d5dba84eb0a06e22729c46
Leonrique <leonrick777@gmail.com>
2018-10-26 20:57:52 -0300

    Retirada de alguns métodos obsoletos.

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -15,17 +15,10 @@
 
       <q-toolbar-title>
         Leon
-        <!-- <div slot="subtitle">Running on Quasar v{{$q.version}}</div> -->
       </q-toolbar-title>
     </q-toolbar>
     
     <div slot="left">
-      <!--
-        Use <q-side-link> component
-        instead of <q-item> for
-        internal vue-router navigation
-      -->
-
       <q-list no-border link inset-delimiter>
         <q-list-header>Essenciais</q-list-header>
         <q-item @click="setView(0)">
@@ -38,7 +31,7 @@
         </q-item>
         <q-item>
           <q-item-side icon="chat" />
-          <q-item-main label="Vazio" sublabel="Aceito sugestões para uma função" />
+          <q-item-main label="Notes" sublabel="Anotações gerais" />
         </q-item>
         <q-item>
           <q-item-side icon="rss feed" />
@@ -46,17 +39,6 @@
         </q-item>
       </q-list>
     </div>
-
-    <!--
-      Replace following <div> with
-      <router-view /> component
-      if using subRoutes
-    -->
-    <!-- <div class="layout-padding logo-container non-selectable no-pointer-events">
-      <div class="logo" :style="position">
-        <img src="~assets/quasar-logo-full.svg">
-      </div>
-    </div> -->
 
     <div v-if="visao == 0" style="margin: 10px">
       <Sorter/>
@@ -89,31 +71,6 @@ import {
 import Sorter from './Sorter.vue'
 import Debts from './Debts.vue'
 
-const { viewport } = dom,
-   { position } = event,
-   moveForce = 30,
-   rotateForce = 40,
-   RAD_TO_DEG = 180 / Math.PI
-
-function getRotationFromAccel(accelX, accelY, accelZ) {
-   /* Reference: http://stackoverflow.com/questions/3755059/3d-accelerometer-calculate-the-orientation#answer-30195572 */
-   const sign = accelZ > 0 ? 1 : -1
-   const miu = 0.001
-
-   return {
-      roll:
-         Math.atan2(
-            accelY,
-            sign * Math.sqrt(Math.pow(accelZ, 2) + miu * Math.pow(accelX, 2))
-         ) * RAD_TO_DEG,
-      pitch:
-         -Math.atan2(
-            accelX,
-            Math.sqrt(Math.pow(accelY, 2) + Math.pow(accelZ, 2))
-         ) * RAD_TO_DEG
-   }
-}
-
 export default {
    name: 'index',
    components: {
@@ -132,13 +89,6 @@ export default {
    },
    data() {
       return {
-         orienting:
-            window.DeviceOrientationEvent && !this.$q.platform.is.desktop,
-         rotating: window.DeviceMotionEvent && !this.$q.platform.is.desktop,
-         moveX: 0,
-         moveY: 0,
-         rotateY: 0,
-         rotateX: 0,
          visao: 0
       }
    },
@@ -159,76 +109,10 @@ export default {
    methods: {
       setView: function(view) {
          this.visao = view
-         this.$refs.layout.toggleLeft()
+         //this.$refs.layout.toggleLeft()
       },
       launch(url) {
          openURL(url)
-      },
-      move(evt) {
-         const { width, height } = viewport(),
-            { top, left } = position(evt),
-            halfH = height / 2,
-            halfW = width / 2
-
-         this.moveX = (left - halfW) / halfW * -moveForce
-         this.moveY = (top - halfH) / halfH * -moveForce
-         this.rotateY = left / width * rotateForce * 2 - rotateForce
-         this.rotateX = -(top / height * rotateForce * 2 - rotateForce)
-      },
-      rotate(evt) {
-         if (
-            evt.rotationRate &&
-            evt.rotationRate.beta !== null &&
-            evt.rotationRate.gamma !== null
-         ) {
-            this.rotateX = evt.rotationRate.beta * 0.7
-            this.rotateY = evt.rotationRate.gamma * -0.7
-         } else {
-            /* evt.acceleration may be null in some cases, so we'll fall back
-           to evt.accelerationIncludingGravity */
-            const accelX =
-                  evt.acceleration.x || evt.accelerationIncludingGravity.x,
-               accelY =
-                  evt.acceleration.y || evt.accelerationIncludingGravity.y,
-               accelZ =
-                  evt.acceleration.z ||
-                  evt.accelerationIncludingGravity.z - 9.81,
-               rotation = getRotationFromAccel(accelX, accelY, accelZ)
-
-            this.rotateX = rotation.roll * 0.7
-            this.rotateY = rotation.pitch * -0.7
-         }
-      },
-      orient(evt) {
-         if (evt.beta === null || evt.gamma === null) {
-            window.removeEventListener('deviceorientation', this.orient, false)
-            this.orienting = false
-
-            window.addEventListener('devicemotion', this.rotate, false)
-         } else {
-            this.rotateX = evt.beta * 0.8
-            this.rotateY = evt.gamma * -0.8
-         }
-      }
-   },
-   mounted() {
-      this.$nextTick(() => {
-         if (this.orienting) {
-            window.addEventListener('deviceorientation', this.orient, false)
-         } else if (this.rotating) {
-            window.addEventListener('devicemove', this.rotate, false)
-         } else {
-            document.addEventListener('mousemove', this.move)
-         }
-      })
-   },
-   beforeDestroy() {
-      if (this.orienting) {
-         window.removeEventListener('deviceorientation', this.orient, false)
-      } else if (this.rotating) {
-         window.removeEventListener('devicemove', this.rotate, false)
-      } else {
-         document.removeEventListener('mousemove', this.move)
       }
    }
 }
